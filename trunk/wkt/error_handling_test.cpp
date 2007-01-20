a5dc659c1371989e3fd5aaf41a1d8fa3c8107241
Mateusz Loskot <mateusz@loskot.net>
2007-01-20 00:59:53 +0000

    Added srouces of OGC Well-Known-Text validator utility.

---
+++ trunk/wkt/error_handling_test.cpp
@@ -0,0 +1,19 @@
+#include "ogc_wkt_grammar.hpp"
+#include <boost/spirit/core.hpp>
+#include <string>
+#include <iostream>
+
+
+int main()
+{
+    std::string wkt("POINT 10 20)");
+    ogc::wkt::grammar gram;
+
+
+    if (parse(wkt.c_str(), gram).full)
+        std::cout << "OK\n";
+    else
+        std::cout << "DUPA\n";
+
+    return 0;
+}
