c30aa425c41d9dddae4c7e0b8af185e48050197d
Mateusz Loskot <mateusz@loskot.net>
2007-04-16 15:03:45 +0000

    Tagging the sid2jp2 0.7 Beta version.

---
+++ sid2jp2/tags/beta-0.7/src/stdafx.cpp
@@ -0,0 +1,12 @@
+// $Id$
+/////////////////////////////////////////////////////////////////////////////
+// 
+// Source file that includes just the standard includes
+// sid2jp2.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+#if (_ATL_VER < 0x0700)
+#include <atlimpl.cpp>
+#endif //(_ATL_VER < 0x0700)
---
+++ sid2jp2/tags/beta-0.7/src/translator.h
@@ -0,0 +1,55 @@
+// $Id$
+/////////////////////////////////////////////////////////////////////////////
+//
+/////////////////////////////////////////////////////////////////////////////
+#ifndef SID2JP2_TRANSLATOR_H_INCLUDED
+#define SID2JP2_TRANSLATOR_H_INCLUDED
+
+#include "dataset.h"
+// gdal
+#include <gdal.h>
+// std
+#include <vector>
+// boost
+#include <boost/noncopyable.hpp>
+
+
+namespace sid2jp2
+{
+extern UINT WM_SID2JP2_FINISH;
+extern UINT WM_SID2JP2_FILE_NEXT;
+extern UINT WM_SID2JP2_FILE_PROGRESS;
+extern UINT WM_SID2JP2_FILE_FAILURE;
+
+class Translator : private boost::noncopyable
+{
+public:
+
+    Translator();
+    ~Translator();
+    void Configure(HWND listener, GDALDriverH driver, char** options, std::vector<dataset_t> const& datasets);
+    void Run();
+    void Terminate();
+    bool IsTerminating() const;
+    HWND GetListener() const;
+
+private:
+    
+    ATL::CWindow m_listener;
+    bool m_running;
+    char** m_options;
+    GDALDriverH m_driver;
+    
+    //std::vector<dataset_t> const& m_datasets;
+    std::vector<dataset_t> m_datasets;
+    
+    void Reset();
+    bool ProcessFile(const char* inputFile, const char* outputFile);
+
+}; // class Translator
+
+int __stdcall TranslationCallback(double complete, const char* msg, void* pArg);
+
+} // namespace sid2jp2
+
+#endif
