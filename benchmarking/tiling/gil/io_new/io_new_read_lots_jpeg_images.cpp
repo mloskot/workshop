5d54edb5c7f55ae0007d506d29af43ad4e549b4f
Mateusz Loskot <mateusz@loskot.net>
2010-12-16 00:16:33 +0000

    Today's update which Christian posted to Boost mailing list.

--- benchmarking/tiling/gil/io_new/io_new_read_lots_jpeg_images.cpp
+++ benchmarking/tiling/gil/io_new/io_new_read_lots_jpeg_images.cpp
@@ -25,6 +25,9 @@
 #include <boost/timer.hpp>
 #include <boost/filesystem/convenience.hpp>
 
+unsigned int counter = 0;
+
+
 #ifdef TEST_IO_NEW
 
 #include <boost/gil/extension/io_new/jpeg_read.hpp>
@@ -35,37 +38,47 @@ using namespace gil;
 
 namespace fs = boost::filesystem;
 
+rgb8_image_t img = rgb8_image_t( 136, 98 );
+
+
+void read_image( const fs::path& dir_path )
+{
+    if ( fs::is_directory( dir_path ) )
+    {
+        fs::directory_iterator end_iter;
+        for( fs::directory_iterator dir_itr( dir_path )
+            ; dir_itr != end_iter
+            ; ++dir_itr
+            )
+        {
+            if (    fs::is_regular( dir_itr->status() ) 
+               && ( fs::extension   ( dir_itr->path() ) == ".jpg" )
+               )
+            {
+                string filename = dir_itr->path().string();
+                read_image( filename, img, jpeg_tag() );
+
+                ++counter;
+            }
+            else if( is_directory(dir_itr->status()) )
+            {
+                read_image( dir_itr->path() );
+            }
+        }
+    }
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
     string in( "C:/chhenning/lots_of_jpegs/" );
-    //string in( "C:/chhenn/ing/few_jpegs/" );
+    //string in( "C:/chhenning/few_jpegs/" );
     fs::path in_path = fs::system_complete( fs::path( in, fs::native ) );
 
     try
     {
-
-        unsigned int counter = 0;
         timer t;
 
-        if ( fs::is_directory( in_path ) )
-        {
-            fs::directory_iterator end_iter;
-            for( fs::directory_iterator dir_itr( in_path )
-                ; dir_itr != end_iter
-                ; ++dir_itr
-                )
-            {
-                if ( fs::is_regular( dir_itr->status() ) 
-                && ( fs::extension( dir_itr->path() ) == ".jpg" ))
-                {
-                    rgb8_image_t img;
-                    string filename = in + string( dir_itr->path().leaf() );
-                    read_image( filename, img, jpeg_tag() );
-
-                    ++counter;
-                }
-            }
-        }
+        read_image( in_path );
 
         cout << "# files : " << counter     << endl;
         cout << "time    : " << t.elapsed() << " sec" << endl;
@@ -88,37 +101,46 @@ using namespace gil;
 
 namespace fs = boost::filesystem;
 
+rgb8_image_t img = rgb8_image_t( 136, 98 );
+
+void read_image( const fs::path& dir_path )
+{
+    if ( fs::is_directory( dir_path ) )
+    {
+        fs::directory_iterator end_iter;
+        for( fs::directory_iterator dir_itr( dir_path )
+            ; dir_itr != end_iter
+            ; ++dir_itr
+            )
+        {
+            if (    fs::is_regular( dir_itr->status() ) 
+               && ( fs::extension   ( dir_itr->path() ) == ".jpg" )
+               )
+            {
+                string filename = dir_itr->path().string();
+                libjpeg_image::read( filename.c_str(), img );
+
+                ++counter;
+            }
+            else if( is_directory(dir_itr->status()) )
+            {
+                read_image( dir_itr->path() );
+            }
+        }
+    }
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
     string in( "C:/chhenning/lots_of_jpegs/" );
-    //string in( "C:/chhenn/ing/few_jpegs/" );
+    //string in( "C:/chhenning/few_jpegs/" );
     fs::path in_path = fs::system_complete( fs::path( in, fs::native ) );
 
     try
     {
-
-        unsigned int counter = 0;
         timer t;
 
-        if ( fs::is_directory( in_path ) )
-        {
-            fs::directory_iterator end_iter;
-            for( fs::directory_iterator dir_itr( in_path )
-                ; dir_itr != end_iter
-                ; ++dir_itr
-                )
-            {
-                if ( fs::is_regular( dir_itr->status() ) 
-                && ( fs::extension( dir_itr->path() ) == ".jpg" ))
-                {
-                    rgb8_image_t jpeg_test_image;
-                    string filename = in + string( dir_itr->path().leaf() );
-                    libjpeg_image::read( filename.c_str(), jpeg_test_image );
-
-                    ++counter;
-                }
-            }
-        }
+        read_image( in_path );
 
         cout << "# files : " << counter     << endl;
         cout << "time    : " << t.elapsed() << " sec" << endl;
