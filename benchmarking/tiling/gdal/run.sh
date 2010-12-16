8323d57e2f4dca397b99e05d2da7c5c97e671d00
Mateusz Loskot <mateusz@loskot.net>
2010-12-16 01:17:07 +0000

    Drop disk caches before each test run

--- benchmarking/tiling/gdal/run.sh
+++ benchmarking/tiling/gdal/run.sh
@@ -5,13 +5,20 @@ fi
 mkdir output
 cd output
 echo "200x200"
+echo 3 | sudo tee -a /proc/sys/vm/drop_caches
+echo 0 | sudo tee -a /proc/sys/vm/drop_caches
 time ../gdal_image_tiles_test_200
 rm -f *png
 echo "256x256"
+echo 3 | sudo tee -a /proc/sys/vm/drop_caches
+echo 0 | sudo tee -a /proc/sys/vm/drop_caches
 time ../gdal_image_tiles_test_256
 rm -f *png
 echo "512x512"
+echo 3 | sudo tee -a /proc/sys/vm/drop_caches
+echo 0 | sudo tee -a /proc/sys/vm/drop_caches
 time ../gdal_image_tiles_test_512
 echo "Done!"
 cd ..
 rm -rf output
+echo 0 | sudo tee -a /proc/sys/vm/drop_caches
