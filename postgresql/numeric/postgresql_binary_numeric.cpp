c57e1d4eb6b0e0b9fe1c9441ee91037cac1575c5
Mateusz Loskot <mateusz@loskot.net>
2011-12-01 02:01:21 +0000

    Fix EOL. Copying header.

--- postgresql/numeric/postgresql_binary_numeric.cpp
+++ postgresql/numeric/postgresql_binary_numeric.cpp
@@ -1,3 +1,9 @@
+//
+// Copyright (C) 2011 Mateusz Loskot <mateusz@loskot.net>
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
 #include <libpq-fe.h>
 #include <algorithm>
 #include <functional>
