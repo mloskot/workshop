7eec35f92504305dfa2fc2308754a9603b1927e3
Mateusz Loskot <mateusz@loskot.net>
2011-12-01 01:57:34 +0000

    Implemented retrival of all properties of NUMERIC value fetched from PGresult in queried as binary format.

--- postgresql/configure.bat
+++ postgresql/configure.bat
@@ -1,13 +1,18 @@
 @ECHO OFF
-REM *** YUR CONFIGURATION ***
-set SRC=G:\dev\mloskot\_git\workshop\postgresql
-set BUILD=G:\dev\mloskot\_git\workshop\postgresql\build
-set BUILD_TYPE=Debug
-set PGSQL=D:\Program Files (x86)\PostgreSQL\9.0
+REM *** CONFIGURATION ***
+set WORKSHOP=D:\dev\_github\workshop
+set MODULE=postgresql
+set PGSQL=C:\Program Files (x86)\PostgreSQL\9.0
 REM ********************************
+set PROJECT=workshop-%MODULE%
+set SRC=%WORKSHOP%\%MODULE%
+set BUILD=%WORKSHOP%\%MODULE%\build
+set BUILD_TYPE=Debug
+set G="Visual Studio 11"
 set PostgreSQL_ROOT=%PGSQL%
 set PATH=%PGSQL%\bin;%PATH%
 IF NOT EXIST %BUILD% mkdir %BUILD%
 cd %BUILD%
-cmake %SRC%
+cmake -G %G% %SRC%
+CALL fix_vs11_sln.bat %PROJECT%.sln
 cd %SRC%
--- postgresql/numeric/postgresql_binary_numeric.cpp
+++ postgresql/numeric/postgresql_binary_numeric.cpp
@@ -1,9 +1,78 @@
 #include <libpq-fe.h>
+#include <algorithm>
 #include <functional>
+#include <iostream>
 #include <memory>
-typedef std::unique_ptr<PGConn, std::function<void(PGConn*)>> connection_t;
+#include <stdexcept>
+#include <vector>
+#include <winsock2.h> // ntohs
+typedef std::unique_ptr<PGconn, std::function<void(PGconn*)>> connection_t;
+typedef std::unique_ptr<PGresult, std::function<void(PGresult*)>> result_t;
+
+connection_t connect(std::string info)
+{
+    connection_t conn(PQconnectdb(info.c_str()), [](PGconn* c) { PQfinish(c); });
+    if (PQstatus(conn.get()) != CONNECTION_OK)
+        throw std::runtime_error("no connection");
+    return conn;
+}
+
+result_t exec(connection_t& conn, std::string sql, bool binary = true)
+{
+    result_t res(PQexecParams(conn.get(), sql.c_str(), 0, 0, 0, 0, 0, binary ? 1 : 0), [](PGresult* r) { PQclear(r); });
+    if (!(PQresultStatus(res.get()) == PGRES_TUPLES_OK || PQresultStatus(res.get()) == PGRES_COMMAND_OK))
+        throw std::runtime_error(PQerrorMessage(conn.get()));
+    return res;
+}
+
+std::vector<short> read_digits(char* val)
+{
+    short* s = reinterpret_cast<short*>(val);
+    int ndigits = ntohs(*s); s++;
+    int weight  = ntohs(*s); s++;
+    int sign    = ntohs(*s); s++;
+    int dscale  = ntohs(*s); s++;
+
+    std::vector<short> digits(ndigits);
+    for (auto i = 0U; i < digits.size(); ++i, s++)
+    {
+        digits[i] = ntohs(*s);
+    }
+    return digits;
+}
+
+template <typename T>
+void echo(T const& t)
+{
+    std::for_each(t.cbegin(), t.cend(), [](short d) { std::cout << d; });
+    std::cout << std::endl;
+}
+
 int main()
 {
-    std::string conninfo;
-    connection_t conn(PQconnectdb(conninfo.c_str(), [&](PGConn* c) {PQfinish(c);});
+    try
+    {
+        std::string conninfo = "dbname=mloskot user=mloskot";
+        connection_t conn = connect(conninfo);
+
+        // num20 => NUMERIC(20, 0)
+        // num202 => NUMERIC(20, 2)
+        result_t res = exec(conn, "SELECT num20, num202 FROM test_data_types LIMIT 1");
+
+        int fn = PQfnumber(res.get(), "num20");
+        char* val = PQgetvalue(res.get(), 0, fn);
+        auto num20 = read_digits(val);
+
+        fn = PQfnumber(res.get(), "num202");
+        val = PQgetvalue(res.get(), 0, fn);
+        auto num202 = read_digits(val);
+
+        // TODO: write digits to string repr, handle all properties
+        echo(num20);
+        echo(num202);
+    }
+    catch (std::exception const& e)
+    {
+        std::clog << e.what() << std::endl;
+    }
 }
