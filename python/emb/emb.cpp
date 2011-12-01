88ad723bfe1671eab66605690ea851aafd21956e
Mateusz Loskot <mateusz@loskot.net>
2011-12-01 00:21:32 +0000

    Implemented example of C++ friendly sys.stdout switching

--- python/emb/emb.cpp
+++ python/emb/emb.cpp
@@ -1,14 +1,47 @@
 #include <functional>
 #include <iostream>
+#include <string>
 #include <Python.h>
 
 namespace emb
 {
 
+typedef std::function<void(std::string)> stdout_write_type;
+
 struct Stdout
 {
     PyObject_HEAD
-    std::function<void(std::string)> callback;
+    stdout_write_type write;
+};
+
+PyObject* Stdout_write(PyObject* self, PyObject* args)
+{
+    std::size_t written(0);
+    Stdout* selfimpl = reinterpret_cast<Stdout*>(self);
+    if (selfimpl->write)
+    {
+        char* data;
+        if (!PyArg_ParseTuple(args, "s", &data))
+            return 0;
+
+        std::string str(data);
+        selfimpl->write(str);
+        written = str.size();
+    }
+    return PyLong_FromSize_t(written);
+}
+
+PyObject* Stdout_flush(PyObject* self, PyObject* args)
+{
+    // no-op
+    return Py_BuildValue("");
+}
+
+PyMethodDef Stdout_methods[] =
+{
+    {"write", Stdout_write, METH_VARARGS, "sys.stdout.write"},
+    {"flush", Stdout_flush, METH_VARARGS, "sys.stdout.write"},
+    {0, 0, 0, 0} // sentinel
 };
 
 PyTypeObject StdoutType =
@@ -34,16 +67,40 @@ PyTypeObject StdoutType =
     0,                    /* tp_as_buffer */
     Py_TPFLAGS_DEFAULT,   /* tp_flags */
     "emb.Stdout objects", /* tp_doc */
+    0,                    /* tp_traverse */
+    0,                    /* tp_clear */
+    0,                    /* tp_richcompare */
+    0,                    /* tp_weaklistoffset */
+    0,                    /* tp_iter */
+    0,                    /* tp_iternext */
+    Stdout_methods,       /* tp_methods */
+    0,                    /* tp_members */
+    0,                    /* tp_getset */
+    0,                    /* tp_base */
+    0,                    /* tp_dict */
+    0,                    /* tp_descr_get */
+    0,                    /* tp_descr_set */
+    0,                    /* tp_dictoffset */
+    0,                    /* tp_init */
+    0,                    /* tp_alloc */
+    0,                    /* tp_new */
 };
 
 PyModuleDef embmodule =
 {
     PyModuleDef_HEAD_INIT,
-    0, 0, 0, 0
+    "emb", 0, -1, 0,
 };
+
+// Internal state
+PyObject* g_stdout;
+PyObject* g_stdout_saved;
 
 PyMODINIT_FUNC PyInit_emb(void) 
 {
+    g_stdout = 0;
+    g_stdout_saved = 0;
+
     StdoutType.tp_new = PyType_GenericNew;
     if (PyType_Ready(&StdoutType) < 0)
         return 0;
@@ -57,13 +114,52 @@ PyMODINIT_FUNC PyInit_emb(void)
     return m;
 }
 
+void set_stdout(stdout_write_type write)
+{
+    if (!g_stdout)
+    {
+        g_stdout_saved = PySys_GetObject("stdout"); // borrowed
+        g_stdout = StdoutType.tp_new(&StdoutType, 0, 0);
+    }
+
+    Stdout* impl = reinterpret_cast<Stdout*>(g_stdout);
+    impl->write = write;
+    PySys_SetObject("stdout", g_stdout);    
+}
+
+void reset_stdout()
+{
+    if (g_stdout_saved)
+        PySys_SetObject("stdout", g_stdout_saved);
+
+    Py_XDECREF(g_stdout);
+    g_stdout = 0;
+}
+
 } // namespace emb
 
 int main()
 {
+    PyImport_AppendInittab("emb", emb::PyInit_emb);
     Py_Initialize();
+    PyImport_ImportModule("emb");
 
-    PyRun_SimpleString("print(3.14)");
+    PyRun_SimpleString("print(\'hello to console\')");
 
+    // switch sys.stdout to custom handler
+    std::string buffer;
+    {
+        emb::stdout_write_type write = [&buffer] (std::string s) { buffer += s; };
+        emb::set_stdout(write);
+        PyRun_SimpleString("print(\'hello to buffer\')");
+        PyRun_SimpleString("print(3.14)");
+        PyRun_SimpleString("print(\'still talking to buffer\')");
+        emb::reset_stdout();
+    }
+
+    PyRun_SimpleString("print(\'hello to console again\')");
     Py_Finalize();
+
+    // output what was written to buffer object
+    std::clog << buffer << std::endl;
 }
