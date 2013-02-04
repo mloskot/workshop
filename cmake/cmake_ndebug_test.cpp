6940f54775658695c4dd06cfc20cb3c53b6d1d6c
Mateusz Loskot <mateusz@loskot.net>
2013-02-04 11:39:59 +0000

    Completed cmake_ndebug_test
    Tested with Visual Studio, NDEBUG=1 for all but Debug builds.

--- cmake/cmake_ndebug_test.cpp
+++ cmake/cmake_ndebug_test.cpp
@@ -1,11 +1,15 @@
-//
 // Does CMake define NDEBUG in non-Debug builds?
-//
+#include "cmake_build_type.h"
 #include <cstdio>
+
 int main()
 {
+    // Possible values are empty, Debug, Release, RelWithDebInfo and MinSizeRel.
+    // This variable is only supported for make based generators.
+    std::printf("CMAKE_BUILD_TYPE=%s\n", CMAKE_BUILD_TYPE);
 #ifdef NDEBUG
+    std::printf("NDEBUG=%d\n", NDEBUG);
 #else
+    std::puts("NDEBUG not defined");
 #endif
-
 }
